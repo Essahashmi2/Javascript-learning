ff38ed8ddf3c24d5f42949f92bbcf19654378fdd
EssaHashmi <essa.hashmi@indusvalleylabs.com>
2021-08-11 14:13:27 +0500

    more programs with multiple methods

---
+++ Chapters/Searchengine/index.js
@@ -0,0 +1,6 @@
+function button() {
+
+    var search = prompt('What Do You Want To Search');
+    
+     window.open('https://www.google.com/search?q=' + search + '&rlz=1CAIUCU_enPK956&oq=what&aqs=chrome..69i57j35i39l2j69i60l2j69i61.1190j0j7&sourceid=chrome&ie=UTF-8');
+    }
